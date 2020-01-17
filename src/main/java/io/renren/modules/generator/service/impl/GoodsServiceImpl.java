6c9ce46d85bdedc4d6b27e901457851eefc6a445
0305chenke <ckke0305@sina.com>
2020-01-17 11:00:16 +0800

    增加新功能

--- src/main/java/io/renren/modules/generator/service/impl/GoodsServiceImpl.java
+++ src/main/java/io/renren/modules/generator/service/impl/GoodsServiceImpl.java
@@ -21,11 +21,28 @@ public class GoodsServiceImpl extends ServiceImpl<GoodsDao, GoodsEntity> impleme
 	public PageUtils queryPage(Map<String, Object> params) {
 		String key = (String) params.get("key");
 		String name = (String) params.get("value");
-		
+		String min = (String) params.get("min");
+		String max = (String) params.get("max");
+		String sort = (String)params.get("sort");
+		Integer iSort = null;
+		if(StringUtils.isNotBlank(sort)) {
+			iSort =  Integer.parseInt(sort);
+		}
 		QueryWrapper<GoodsEntity> queryWrapper = new QueryWrapper<>();
 		queryWrapper
 			.eq(StringUtils.isNotBlank(name),"name", name)
-			.like(StringUtils.isNotBlank(key),"intro", key);
+			.like(StringUtils.isNotBlank(key),"intro", key)
+			.between(StringUtils.isNotBlank(min) && StringUtils.isNotBlank(max), "price", min, max)
+			.gt(StringUtils.isNotBlank(min),  "price", min)
+			.le(StringUtils.isNotBlank(max), "price", max);
+		if(iSort!=null) {
+			queryWrapper
+			.orderByDesc(iSort==1, "price")
+			.orderByAsc(iSort==2, "price")
+			.orderByDesc(iSort==3, "num")
+			.orderByAsc(iSort==4, "num");
+		}
+			
 
 		IPage<GoodsEntity> page = this.page(new Query<GoodsEntity>().getPage(params), queryWrapper);
 
--- src/main/java/io/renren/modules/job/task/MyTask.java
+++ src/main/java/io/renren/modules/job/task/MyTask.java
@@ -13,6 +13,9 @@ public class MyTask  implements ITask{
 	public void run(String params) {
 		logger.debug("自定义的定时任务正在执行，参数为：{}", params);
 		System.out.println("这是自定义的定时任务");
+		for(int i = 0; i<1000; i++) {
+			System.out.println("这是自定义的定时任务第"+i+"个执行方法");
+		}
 		
 	}
 
